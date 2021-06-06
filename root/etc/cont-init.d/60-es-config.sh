08e27ab7175252f21d7cc6fcc9c0d2108e2d84fd
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-06-06 10:14:22 -0500

    Fix url config

--- root/etc/cont-init.d/60-es-config.sh
+++ root/etc/cont-init.d/60-es-config.sh
@@ -10,13 +10,12 @@ if [ ! -f "/config/zmeventnotification.ini" ]; then
 fi
 
 echo "Setting ES ZoneMinder URL" | info "[${program_name}] "
-fdmove 1 100 \
-  python3 -u /zoneminder/config_edit.py \
-      --config /config/secrets.ini \
-      --output /config/secrets.ini \
-      --set \
-          secrets:ZMES_PICTURE_URL="https://${ES_COMMON_NAME}/index.php?view=image\&eid=EVENTID\&fid=objdetect\&width=600" \
-          secrets:ZM_PORTAL="https://${ES_COMMON_NAME}"
+python3 -u /zoneminder/config_edit.py \
+    --config /config/secrets.ini \
+    --output /config/secrets.ini \
+    --set \
+        secrets:ZMES_PICTURE_URL="https://${ES_COMMON_NAME}/index.php?view=image\&eid=EVENTID\&fid=objdetect\&width=600" \
+        secrets:ZM_PORTAL="https://${ES_COMMON_NAME}"
 
 echo "Configuring ZoneMinder Common Name in Nginx Config" | info "[${program_name}] "
 sed -i "s|ES_COMMON_NAME|${ES_COMMON_NAME}|g" /etc/nginx/conf.d/ssl.conf
